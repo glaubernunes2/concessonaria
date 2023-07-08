3e3fb19041568653f95635d96f02d9cfdf9d509d
glauber nunes <119985120+glaubernunes2@users.noreply.github.com>
2023-07-08 10:48:49 -0300

    atulização

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "web-conce.capuj8qhlhhb.sa-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "04okkhDgYkGORO9kgc8g";
+$database = "db_conce";
+?>
